f9d6f56c11ebb8a6b4513ddd5f2df4e8834851ac
Pete Hanner <petehanner@gmail.com>
2019-12-08 18:42:30 -0500

    non OO solution;

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,13 @@
 # Implement your procedural solution here!
+def smallest_multiple(num)
+  divisors = *(2..num)
+  i = num
+  nondivisible = true
+
+  while nondivisible
+    nondivisible = false if divisors.all? { |div| i % div == 0 }
+    i += num if nondivisible
+  end
+  
+  return i
+end
